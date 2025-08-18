addc294e5707cd272d1ccc43a5aa73feae2144f2
KabweRuth <202100124@my.apiu.edu>
2025-08-18 14:27:50 +0700

    chage

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Web Page</title>
+    <head>git works</head>
+    <head>
+         <?php
+        echo "Welcome to My Web Page! <br>";
+        echo "This is a simple HTML document.<br>";
+
+        $X = 5;
+        $Y = 2.5;
+        $name = "Kabwe Ruth";
+        $array = [20,22,30,33]   ;
+
+        echo $X;
+        define("PI", 3.14); 
+        echo PI;
+
+        ?>
+
+
+    </head>
+    
+    <link rel="stylesheet" href="styles.css">
+
+</html>
---
+++ php.php
@@ -0,0 +1,14 @@
+ <?php
+        echo "Welcome to My Web Page!";
+        echo "br";
+        echo "This is a simple HTML document.";
+
+        $X = 5;
+        $Y = 2.5;
+        $name = "Kabwe Ruth";
+        $array = [20,22,30,33]   ;
+
+        echo $X;
+
+        ?>
+
