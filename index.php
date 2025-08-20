b6f285dafdd35a758e1b79f67a667114ea33e0aa
KabweRuth <202100124@my.apiu.edu>
2025-08-20 14:19:18 +0700

    comm

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html> 
+<!-- <!DOCTYPE html> 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,9 +6,16 @@
     <title>My Web Page</title>
     <head>git works</head>
     <head>
-         <?php
-        echo "Welcome to My Web Page! <br>";
-        echo "This is a simple HTML document.<br>";
+        
+    </head>
+    
+    <link rel="stylesheet" href="styles.css">
+
+</html> -->
+ <?php
+        echo "Welcome to My Web Page!";
+        echo "br";
+        echo "This is a simple HTML document.";
 
         $X = 5;
         $Y = 2.5;
@@ -16,14 +23,6 @@
         $array = [20,22,30,33]   ;
 
         echo $X;
-        define("PI", 3.14); 
-        echo PI;
 
         ?>
 
-
-    </head>
-    
-    <link rel="stylesheet" href="styles.css">
-
-</html>
